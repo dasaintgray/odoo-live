cb537a05c573a4afbc04665e5078cfa9b7c91dbd
Menyek <menyek@circuitmindz.com>
2022-02-15 01:13:46 +0000

    push product view

--- addons/imerex_multi_branch/static/src/js/pos_branch.js
+++ addons/imerex_multi_branch/static/src/js/pos_branch.js
@@ -6,11 +6,8 @@ odoo.define('imerex_multi_branch.branch', function(require){
         model: 'res.branch',
         fields: ['id', 'street', 'street2','city','state_id','country_id','email','phone','website'],
         domain: [['id', '=', this.pos.config.branch_id[0]]],
-        // loaded: function (self, branch_info) {
-        //     _.each(branch_info, function (product) {
-        //         self.db.on_hand_qty[product['id']] = [product['qty_available'], product['virtual_available'],0,0]
-        //     })
-        // }
+        ids:    function(self){ return [self.session.user_context.allowed_company_ids[0]]; },
+        loaded: function(self,companies){ self.company = companies[0]; },
     })
 
 
